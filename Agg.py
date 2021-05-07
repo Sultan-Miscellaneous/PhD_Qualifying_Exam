e258ea02c9ac1364d2a77430864e823402c454ff
asultan123 <asultan@aucegypt.edu>
2021-05-07 15:53:06 -0400

    Validated AGG

--- Agg.py
+++ Agg.py
@@ -1,6 +1,7 @@
 # %%
 from myhdl import block, Signal, delay, always, instance
 from tqdm.contrib.itertools import product
+from itertools import product as silent_product
 
 @block
 def agg(clk, enable, input_0, input_1, output):
@@ -19,10 +20,13 @@ def agg_tb():
     input_1 = Signal(0)
     output_0 = Signal(0)
     enable = Signal(0)
-    stop_sim = Signal(0)
+    stop_sim = Signal(False)
 
     dut = agg(clk, enable, input_0, input_1, output_0)
 
+    stimulus_testcases = product(range(0,100), range(0,100), desc="Pushing input into AG")
+    monitor_testcases = silent_product(range(0,100), range(0,100))
+    
     @instance
     def clk_driver():
         while True:
@@ -35,21 +39,28 @@ def agg_tb():
     @instance
     def stimulus():
         enable.next = True
-        stop_sim.next = False
         yield clk.posedge
-        for i,j in product(range(100), range(100)):
+        for i,j in stimulus_testcases:
             input_0.next = i
             input_1.next = j
             yield clk.posedge
-            if(output_0.val != i+j):
+        stop_sim.next = True
+        yield clk.posedge # last input
+        
+        
+    @instance
+    def monitor():
+        yield enable
+        while not stop_sim:
+            expected_next = next(monitor_testcases)
+            if(output_0.val != sum(expected_next)):
                 raise Exception("Agg testbench failed... aborting...")
-        print("Agg testbench pass")
-        
-        
-    return clk_driver, stimulus, dut
+            yield output_0, stop_sim
+        print("\n Agg testbench pass")
+    return clk_driver, monitor, stimulus, dut
 
  
 if __name__ == '__main__':
     print("Running AGG testbench")
     inst = agg_tb()
-    inst.run_sim()
+    inst.run_sim(quiet=True)

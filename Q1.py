91cde02d33a1785e266227e35c51019066e29411
asultan123 <asultan@aucegypt.edu>
2021-05-07 05:09:04 -0400

    added AGG and Datamover, both need testing

--- Q1.py
+++ Q1.py
@@ -95,6 +95,41 @@ def memory_tb():
     return clk_driver, testbench
  
 @block
+def agg(clk, enable, input_0, input_1, output):
+
+    @always(clk.posedge)
+    def compute():
+        if enable:
+            output.next = input_0.val + input_1.val
+
+    return compute
+
+@block
+def datamover(clk, enable, data, port, program, mode = 'read'):
+    
+    def apply_access(iteration_vector, access_map):
+        access_idx = 0
+        for iteration_dim, constant in zip(iteration_vector, access_map[:-1]):
+            access_idx += (iteration_dim*constant)
+        access_idx += access_map[-1]
+        return access_idx
+ 
+    @always(clk.posedge)
+    def access_mem():
+        if enable:
+            for (domain, access_map, condition) in program:
+                for iteration_vector in domain:
+                    if condition(iteration_vector):
+                        access_idx = apply_access(iteration_vector, access_map)
+                        if mode == 'read':
+                            data.next = port(access_idx)
+                        else:
+                            port(access_idx, data.val)
+                        
+    
+    return access_mem
+ 
+@block
 def pe(clk, enable, pad_origin, pads, weights, ifmap_in, ofmap_out):
 
     pad_completion_counter = Counter()

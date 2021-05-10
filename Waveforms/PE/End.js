895f36f937197f38f8e1d1250ee9c3c6c3ca5f25
asultan123 <asultan@aucegypt.edu>
2021-05-10 01:59:25 -0400

    Hearbeat Wavedroms

--- Waveforms/PE/End.js
+++ Waveforms/PE/End.js
@@ -1,7 +1,7 @@
 { signal: [
   { name: "clk", wave: 'P......' },
   { name: 'dram_read_p_0 [ifmap]', wave: '34x..|.', data: ["[223, 222]", "[223, 223]"] },
-  { name: "Conv_ ]3_3 [state]", wave: '=..=.|.', data: ["Compute", "Pad for 450 cycles                          "]},
+  { name: "Conv_ ]3_3 [state]", wave: '=..=.|.', data: ["Compute", "Pad for 224 cycles                          "]},
   { name: "Conv_3_3 [ofmap]", wave: '=34=.|.', data: ["[221, 219]", "[221, 220]", "[221, 221]", "0"]},
   ],
   config: { hscale: 1.5 }
